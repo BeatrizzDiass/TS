83e430872788eb18f59b3d72a7527659aa81767b
BeatrizzDiass <2241609@my.ipleiria.pt>
2025-03-11 17:32:50 +0000

    Ficha 1

---
+++ Ficha1/Ficha1/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Ficha1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.LabelCopyMessage = new System.Windows.Forms.Label();
+            this.progressBarCopyFile = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(315, 98);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 68);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Copy File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // LabelCopyMessage
+            // 
+            this.LabelCopyMessage.AutoSize = true;
+            this.LabelCopyMessage.Location = new System.Drawing.Point(351, 246);
+            this.LabelCopyMessage.Name = "LabelCopyMessage";
+            this.LabelCopyMessage.Size = new System.Drawing.Size(136, 16);
+            this.LabelCopyMessage.TabIndex = 1;
+            this.LabelCopyMessage.Text = "Copy Message Label";
+            this.LabelCopyMessage.Visible = false;
+            // 
+            // progressBarCopyFile
+            // 
+            this.progressBarCopyFile.Location = new System.Drawing.Point(315, 195);
+            this.progressBarCopyFile.Name = "progressBarCopyFile";
+            this.progressBarCopyFile.Size = new System.Drawing.Size(200, 23);
+            this.progressBarCopyFile.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.progressBarCopyFile);
+            this.Controls.Add(this.LabelCopyMessage);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label LabelCopyMessage;
+        private System.Windows.Forms.ProgressBar progressBarCopyFile;
+    }
+}
+
---
+++ Ficha1/Ficha1/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ficha1
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //definir as variaveis 
+            int buffersize = 2048;
+            byte[] buffer = new byte[buffersize];
+
+            int bytesRead = 0;
+
+            String originalpath = "security.jpg";
+            String copyFilepath = "copy_security.jpg";
+
+
+            //Extra 2 - gerar um ficheiro log com as ações realizadas
+            String logFilePath = "log.txt";
+            StreamWriter logStreamWriter = new StreamWriter(logFilePath, true);
+
+            
+            //verificar se o ficheiro existe
+            if (File.Exists(copyFilepath))
+            {
+                File.Delete(copyFilepath);
+            }
+
+            //criar controladores de ficheiros
+            FileStream originalFileStream = new FileStream(originalpath, FileMode.Open);
+            FileStream copyFileStream = new FileStream(copyFilepath, FileMode.Create);
+
+            //definir os limites da progressBar
+            progressBarCopyFile.Minimum = 0;
+            progressBarCopyFile.Maximum = (int)originalFileStream.Length;
+            progressBarCopyFile.Step = buffersize;
+
+            //copiar o conteúdo
+            while ((bytesRead = originalFileStream.Read(buffer, 0, buffersize)) >0)
+            {
+                System.Threading.Thread.Sleep(1000);
+                copyFileStream.Write(buffer, 0, bytesRead);
+                progressBarCopyFile.PerformStep();
+            }
+
+            //mostrar a mensagem ao utilizador
+            String msg = "Ficheiro copiado [" + originalFileStream.Length + " bytes]\r\n";
+            LabelCopyMessage.Text = msg;
+            LabelCopyMessage.Visible = true;
+
+            //escrever um ficheiro log
+            logStreamWriter.WriteLine(msg);
+
+            //libertar recuros
+            originalFileStream.Close();
+            copyFileStream.Close();
+            
+            //Extra 2
+            logStreamWriter.Close();
+
+        
+        
+        
+        
+        }
+    }
+}
